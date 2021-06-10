cb09a86ba7f0f58872ae6d93a82ceb388d41f024
Abu Siddique <asrafeee@yahoo.com>
2021-06-09 20:32:38 -0400

    responsive done

--- components/Results.js
+++ components/Results.js
@@ -1,15 +1,16 @@
+import FlipMove from 'react-flip-move'
 import Thumbnail from "./Thumbnail"
 
 function Results({results}) {
     return (
-        <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
+        <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
             {
                 results.map(result=>(
                     <Thumbnail key={result.id} result={result}/>
                     // console.log(result);
     ))
             }
-        </div>
+        </FlipMove>
     ) 
 }
 
